6c4f32aec83d09e8f982ae498a86d5d185245e54
chrishvk <71586554@continental.edu.pe>
2022-11-22 00:18:59 -0500

    Primer commit

---
+++ operadores.py
@@ -0,0 +1,13 @@
+# Área de un triángulo
+# Datos:
+'''
+b = base
+h = altura
+
+area = (b * h) / 2
+'''
+
+b = float(input('Ingrese tamaño de la base: '))
+h = float(input('Ingrese tamaño de la altura: '))
+area = (b * h) / 2
+print('El resultado del área del triángulo es: ' + str(area))
---
+++ suma.py
@@ -0,0 +1,4 @@
+a = int(input('Ingrese un número: '))
+b = int(input('Ingrese otro número: '))
+suma = a + b
+print('La suma es ' + str(suma))
